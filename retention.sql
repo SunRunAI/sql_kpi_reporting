7a90e78982f1302f6b6c9c3f161ebd47f8e4e622
Michael Watson <michaelwatson747@gmail.com>
2021-02-28 10:39:55 +0000

    Added full date index to retention script output

--- retention.sql
+++ retention.sql
@@ -4,13 +4,14 @@ A user is considered to be active in a given period if
 they enter at least 1 league.
 */
 
--- Set hardcoded constant
-WITH obs_period AS (
+-- Set constant
+WITH const AS (
     SELECT '2019-09-01'::DATE AS start_date
     ),
 -- Get deltas between times played, and start of observation period
 user_activity AS (
     SELECT  display_name AS user,
+            t.date AS date,
             (DATE_PART('year', t.date) 
             - DATE_PART('year', start_date)) 
             * 12
@@ -18,7 +19,7 @@ user_activity AS (
             - DATE_PART('month', start_date))
             AS month
     FROM transactions AS t 
-    CROSS JOIN obs_period
+    CROSS JOIN const
     WHERE transaction_type = 'League Entry'
         AND date >= start_date
     ),
@@ -50,9 +51,17 @@ retention AS (
         WHEN delta >= 2 THEN 'lagged'
         END AS cust_state
         FROM time_deltas AS t
-    )
+    ),
+-- Grab full month commencing dates
+full_dates AS (
+    SELECT month,
+           DATE_TRUNC('month', MIN(date)) AS full_date
+    FROM user_activity
+    GROUP BY month
+)
 -- Calculate retention rate
-SELECT month,
+SELECT r.month,
+       MIN(f.full_date) AS date,
        COUNT(DISTINCT r.user) AS num_users,
        SUM(CASE WHEN cust_state = 'retained' THEN 1
            ELSE 0 END)
@@ -61,4 +70,6 @@ SELECT month,
            ELSE 0 END)::DECIMAL / COUNT(DISTINCT r.user)
        AS retention_rate
        FROM retention AS r
-       GROUP BY month;
+       INNER JOIN full_Dates AS f
+       ON r.month = f.month
+       GROUP BY r.month;
